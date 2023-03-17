dd5bcad7ce0b68fcdca862582717574dd0f86f8c
olgierdxyz@gmail.com <olgierdxyz@gmail.com>
2023-03-17 16:25:58 +0100

    ...

--- vars/myStep.groovy
+++ vars/myStep.groovy
@@ -1,7 +1,8 @@
 import java.util.UUID
 
 def call() {
-    System.out.println("Hello from call!")
-    def uuid = UUID.randomUUID().toString()
-    return [MY_RANDOM_STRING: uuid]
+    //System.out.println("Hello from call!")
+    //def uuid = UUID.randomUUID().toString()
+    //return [MY_RANDOM_STRING: uuid]
+    return "Hello from call!"
 }
